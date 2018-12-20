3dcbf312dceb18c8a588a2890630ce4037305311
Bhishan Poudel <bhishanpdl@users.noreply.github.com>
2018-12-20 12:59:35 -0500

    Add files via upload

---
+++ run_clusters/clusters_hdf5_simtxt.py
@@ -0,0 +1,43 @@
+import numpy as np
+import glob
+
+from astropy.io import fits
+from astropy.table import Table
+
+# read fitstable
+fitstable_path = glob.glob('output/src/*/*/src.fits')[0]
+print('Reading: {}'.format(fitstable_path))
+
+# astropy table
+t = Table.read(fitstable_path,format='fits')
+
+# RA and DEC pixels
+ra_pix = t['base_NaiveCentroid_x']
+dec_pix = t['base_NaiveCentroid_y']
+
+# RA and Dec values
+ra = np.abs(ra_pix*0.2/3600.-0.2) # assumes a 0.2 arcmin pixel size
+dec = dec_pix*0.2/3600.
+
+# ellipticities
+e1 = t['ext_shapeHSM_HsmShapeRegauss_e1']
+e2 = t['ext_shapeHSM_HsmShapeRegauss_e2']
+
+# object id
+obj_id = t['id']
+
+# Write pipeline-compatible hdf5 file
+hdf5 = 'sim.hdf5'
+names = ('id', 'coord_ra_deg', 'coord_dec_deg', 'ext_shapeHSM_HsmShapeRegauss_e1', 'ext_shapeHSM_HsmShapeRegauss_e2')
+
+t2 = Table([obj_id, ra, dec, e1, e2], names=names)
+
+print('Writing: {}'.format(hdf5))
+t2.write(hdf5, path='deepCoadd_meas', overwrite=True, format='hdf5')
+
+# Write sim.txt file
+zsim = np.zeros(len(ra))+1.5
+data = np.array([obj_id, zsim])
+
+print('Writing: sim.txt')
+np.savetxt('sim.txt', data.T, fmt=['%i','%f'])
